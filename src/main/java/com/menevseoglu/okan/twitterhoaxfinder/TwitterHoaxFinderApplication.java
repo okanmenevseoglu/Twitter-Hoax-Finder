a876dc328f1d40f368a7d838bdb7bfbc1d2c886a
Okan Menevseoglu <okan.menevseoglu@dolap.com>
2017-10-15 16:02:07 +0300

    Initial commit.

---
+++ src/main/java/com/menevseoglu/okan/twitterhoaxfinder/TwitterHoaxFinderApplication.java
@@ -0,0 +1,12 @@
+package com.menevseoglu.okan.twitterhoaxfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterHoaxFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterHoaxFinderApplication.class, args);
+	}
+}
