de73525948b28afec39e5d8cd9d244126ae6f11d
Okan Menevseoglu <okan.menevseoglu@dolap.com>
2017-12-05 19:52:01 +0300

    add routing ts

---
+++ twitter-hoax-finder-client/src/app/shared/config/api-route.ts
@@ -0,0 +1,21 @@
+export class ApiRoute {
+
+  private static baseEndPoint: string = 'http://localhost:8080/';
+
+  static getHealthCheckURL(): string {
+    return this.baseEndPoint;
+  }
+
+  static getSearchTextURL(): string {
+    return this.baseEndPoint + 'search/text';
+  }
+
+  static getSearchTextByMemberURL(): string {
+    return this.baseEndPoint + 'search/text/by-member';
+  }
+
+
+  public static getSearchImageURL(): string {
+    return this.baseEndPoint + 'search/image';
+  }
+}
